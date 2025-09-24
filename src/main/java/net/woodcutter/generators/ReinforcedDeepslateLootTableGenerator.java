1fb0b107fdb349258406f2e6f4b9da769841e3cd
Marcel Solle <msap3rtur3@gmail.com>
2025-09-24 20:09:16 +0200

    feat: add deepslate recipe models & textures

---
+++ src/main/java/net/woodcutter/generators/ReinforcedDeepslateLootTableGenerator.java
@@ -0,0 +1,17 @@
+package net.woodcutter.generators;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ReinforcedDeepslateLootTableGenerator extends FabricBlockLootTableProvider {
+    protected ReinforcedDeepslateLootTableGenerator(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+    }
+}
